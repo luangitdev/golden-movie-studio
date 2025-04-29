5b64a86b1a235deb93f98cb8f8f8953179d5e292
luangitdev <luaninveste@gmail.com>
2025-04-29 01:15:03 -0300

    feat: atualizar códigos

--- cypress/e2e/buscar.cy.js
+++ cypress/e2e/buscar.cy.js
@@ -1,5 +1,27 @@
- /// <reference types="cypress" />
-  
- beforeEach(() => {
+/// <reference types="cypress" />
+
+beforeEach(() => {
     cy.visit('/')
-  })
+})
+
+it('Deve fazer o cadastro de campos obrigatórios', () => {
+    cy.get('#search-input').type('Matrix')
+    cy.get('#search-button').click()
+    cy.get('#results-section').should('contain', 'Matrix')
+});
+
+it('Deve buscar filmes com sucesso de uma lista', () => {
+    cy.fixture('filmes.json').then((filmes) => {
+        cy.get('#search-input').type(filmes[0].titulo)
+        cy.get('#search-button').click()
+        cy.get('#results-section').should('contain', filmes[0].titulo)
+    });
+});
+
+it.only('Deve buscar filmes com sucesso de uma lista inteira', () => {
+    cy.fixture('filmes.json').each((filmes) => {
+        cy.get('#search-input').clear().type(filmes.titulo)
+        cy.get('#search-button').click()
+        cy.get('#results-section').should('contain', filmes.titulo)
+    });
+});
