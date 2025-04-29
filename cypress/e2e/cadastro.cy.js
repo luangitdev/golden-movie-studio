5fd5bbd94ffa5d286abdd3814acfef696092c3b9
luangitdev <luaninveste@gmail.com>
2025-04-29 00:33:25 -0300

    feat: renomeado arquivo spec.cy.js para cadastro.cy.js e estruturado o arquivo commands.js

---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,28 @@
+describe('US-012-Funcionalidade: Cadastro de membros', () => {
+  /// <reference types="cypress" />
+  
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('Deve fazer o cadastro de campos obrigatórios', () => {
+    var email = `luan${Date.now()}@email.com`
+    cy.preencherCadastro('Luan', 'Silva', email, '11999999999', 'Teste@123456')
+    cy.get('#signup-response').should('contain', 'Cadastro realizado com sucesso!')
+  })
+
+  it('Deve validar a mensagem de erro com o campo nome inválido', () => {
+    cy.preencherCadastro('Luan20', 'Silva', 'luan@email.com', '11999999999', 'Teste@123456')
+    cy.get('#signup-response').should('contain', 'Nome deve conter apenas caracteres alfabéticos, acentuados e espaços')
+  })
+
+  it('Deve validar a mensagem de erro com o campo email inválido', () => {
+    cy.preencherCadastro('Luan', 'Silva', 'luanemail.com', '11999999999', 'Teste@123456')
+    cy.get('#signup-response').should('contain', 'E-mail deve ser um email válido')
+  })
+
+  it.only('Deve validar a mensagem de erro com o campo senha fraca', () => {
+    cy.preencherCadastro('Luan', 'Silva', 'luan@email.com', '11999999999', '123456')
+    cy.get('#signup-response').should('contain', 'Senha deve ter pelo menos 8 caracteres, incluir uma letra maiúscula')
+  })
+})
